306943e1fc2388efc405e29d8512218b1b11456b
kinuko1221 <kinuko1221@net44-dhcp81.sfc.keio.ac.jp>
2016-05-19 10:57:02 +0900

    練習問題1

--- campaign.js
+++ campaign.js
@@ -17,10 +17,10 @@ function campaignNumber(age, sex, married){
     if(age >= 30 && sex == 1 && married == true) {
      return 1;   
     }
-    if(20 <= age > 30 && sex == 0 && married == false) {
+    else if(20 <= age > 30 && sex == 0 && married == false) {
        return 2;
     }
-    if(sex == 0 && married == true){
+    else if(sex == 0 && married == true){
         return 3;
     }
     else{
