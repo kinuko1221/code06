2ce0afdff4a6bef99953d1eaa686c6c1ca99d1fb
kinuko1221 <kinuko1221@net44-dhcp81.sfc.keio.ac.jp>
2016-05-19 10:27:08 +0900

    練習問題1

--- campaign.js
+++ campaign.js
@@ -10,4 +10,20 @@ const CAMPAIGN_MARRIED_FEMALE = 3;
 const CAMPAIGN_OTHERS = 4;
 
 function campaignNumber(age, sex, married){
-}
+    var age = input (age)
+    var sex = input (sex)
+    var married = input (married)
+    
+    if(age >= 30 && sex == 1 && married == true) {
+      CAMPAIGN_MARRIED_MALE(show"1");   
+    }
+    if(20 <= age > 30 && sex == 0 && married == false) {
+        CAMPAIGN_UNMARRIED_FEMALE(show"2");
+    }
+    if(sex == 0 && married == true){
+        CAMPAIGN_MARRIED_FEMALE(show"3")
+    }
+    else{
+        CAMPAIN_OTHERS(show"4")
+    }
+
